61687aa7181ae26344899ba3c0e5aa410e787417
iTBoyer <iTBoyer@qq.com>
2022-10-16 21:42:04 +0800

    现象：termux不支持swift run，使用unit开发测试 原因： 处理办法：

--- Sources/UnitLib/Test.swift
+++ Sources/UnitLib/Test.swift
@@ -3,11 +3,3 @@ public func hello()
     print("hello world---")
     print("hello world-----")
 }
-
-func factorial(n: Int) -> Int {
-    if n <= 1 { return n }
-    return n * factorial(n: n - 1)
-}
-
-let number = 4
-print("\(number)! is equal to \(factorial(n: number))")
