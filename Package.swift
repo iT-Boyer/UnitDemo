b6460e03a4d57f7356a07b419c0e797ab1c725d9
iTBoyer <iTBoyer@qq.com>
2022-10-16 19:40:05 +0800

    现象：updaate 在termux中使用单元测试，和终端命令。不要在奢求fastlane和 nimble的想法 原因： 处理办法：

--- Package.swift
+++ Package.swift
@@ -10,15 +10,16 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
       .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
       .package(url: "https://github.com/Quick/Quick.git", from: "5.0.0"),
-//      .package(url: "https://github.com/Quick/Nimble.git", from: "10.0.0")
-      .package(url: "https://github.com/it-boyer/fastlane.git", branch: "public"),
+      //.package(url: "https://github.com/Quick/Nimble.git", from: "10.0.0"),
+      //.package(url: "https://github.com/it-boyer/fastlane.git", branch: "public"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "UnitDemo",
-            dependencies: [.product(name: "Fastlane", package: "fastlane"),
+            dependencies: [
+                        //.product(name: "Fastlane", package: "fastlane"),
                            .product(name: "ArgumentParser", package: "swift-argument-parser")]),
         .testTarget(
             name: "UnitDemoTests",
