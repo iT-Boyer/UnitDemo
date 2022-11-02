5f7ff5973156e6d89b8aeeaa78c171a43ec97dde
iTBoyer <iTBoyer@qq.com>
2022-11-03 00:27:01 +0800

    现象：update 原因： 处理办法：

---
+++ Sources/UnitLib/Calculate.swift
@@ -0,0 +1,14 @@
+//开启设计模式第一堂课
+//编写一个简单的程序，培养封装思想的思维转换。
+//面向对象：封装，继承，多态
+//计算器的功能，要便于扩展，维护，健壮
+//案例：输入两个数字和运算符得出结果
+//面向对象的设计思想
+
+public class Cal
+{
+
+    static public func cal(first:Double,second:Double) -> Double {
+        return first + second
+    }
+}
